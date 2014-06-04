997504e003bde7e4e9c10f598964e16df7d72b45
Damien <damien@teamwork.com>
2014-06-04 11:45:46 +0100

    new
    new

---
+++ test/index.js
@@ -0,0 +1,9 @@
+var mocha = require('mocha');
+var chai = require('chai');
+var expect = chai.expect;
+
+describe('Something', function(){
+	it('should do something', function(){
+		expect(true).to.equal(false);
+	});
+});
