09413b0f3ec7a9a176c5c8672525a1d081b7e571
Damien <damien@teamwork.com>
2014-06-05 13:36:20 +0100

    delay
    delay

--- test/index.js
+++ test/index.js
@@ -1,7 +1,7 @@
 var mocha = require('mocha');
 var chai = require('chai');
 var expect = chai.expect;
-var delay=1000;//1 seconds
+var delay=2000;//1 seconds
     setTimeout(function(){
 describe('Something', function(){
 	it('should do something', function(){
