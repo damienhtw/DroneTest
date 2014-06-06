6c4533bafb9fdce10b08ca0217f77c2c0d9d144d
Damien <damien@teamwork.com>
2014-06-06 12:44:33 +0100

    drone ubuntu server
    drone ubuntu server

--- test/index.js
+++ test/index.js
@@ -10,11 +10,6 @@ describe('Something', function(){
 
 //this is an added comment
 //this is an added comment
-//this is an added comment
-//this is an added comment
-//this is an added comment
-//this is an added comment
-//this is an added comment
-//this is an added comment
+
 
 
